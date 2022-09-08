775aba6d8cabed92200f82e2c097a0df687569f1
Bruno Martins <martins_bruno01@hotmail.com>
2022-09-08 01:01:24 -0300

    Heroku - Teste resolução do problema

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -37,8 +37,8 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 
-		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " +
-				  date;
+		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
+			    + " com um total de R$ " + String.format("%.0f", sale.getAmount());
 		
 		/*
 		 * String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " +
