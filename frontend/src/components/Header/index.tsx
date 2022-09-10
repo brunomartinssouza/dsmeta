ae5cc480ae2ac738b5ee1768447cec7bd0096c9d
Bruno Martins <martins_bruno01@hotmail.com>
2022-09-10 01:58:59 -0300

    Ajustes

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,8 +9,8 @@ function Header() {
             <img src={logo} alt="DSMeta" />
             <h1>DSMeta</h1>
             <p>
-              Desenvolvido por
-              <a href="https://github.com/brunomartinssouza">/brunomartinssouza</a>
+              Desenvolvido por 
+              <a href="https://github.com/brunomartinssouza"> /brunomartinssouza</a>
             </p>
         </div>
     </header>
